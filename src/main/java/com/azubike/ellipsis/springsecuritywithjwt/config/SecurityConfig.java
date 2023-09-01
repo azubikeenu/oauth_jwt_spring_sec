a1b6d352b64bc0a9ddc484a42161b041f089a74d
Enu Azubike <enuazubike88@gmail.com>
2023-09-02 00:00:46 +0100

    updated token endpoint to retreive authentication data from request body

--- src/main/java/com/azubike/ellipsis/springsecuritywithjwt/config/SecurityConfig.java
+++ src/main/java/com/azubike/ellipsis/springsecuritywithjwt/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.azubike.ellipsis.springsecuritywithjwt.config;
 
+import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
@@ -8,55 +9,91 @@ import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
-    private final RsaKeyConfig rsaKeyConfig;
+//    private final RsaKeyConfig rsaKeyConfig;
+//
+//    public SecurityConfig(final RsaKeyConfig rsaKeyConfig) {
+//        this.rsaKeyConfig = rsaKeyConfig;
+//    }
 
-    public SecurityConfig(final RsaKeyConfig rsaKeyConfig) {
-        this.rsaKeyConfig = rsaKeyConfig;
-    }
+    private RSAKey rsaKey;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/token").permitAll().anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-                .httpBasic(Customizer.withDefaults())
+                .exceptionHandling(ex ->
+                        ex.authenticationEntryPoint( new BasicAuthenticationEntryPoint())
+                                .accessDeniedHandler(new BearerTokenAccessDeniedHandler()))
                 .build();
     }
 
 
     @Bean
-    public JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withPublicKey(rsaKeyConfig.publicKey()).build();
+    public AuthenticationManager authenticationManager (UserDetailsService userDetailsService){
+        final DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        return new ProviderManager(authProvider);
+    }
+
+
+ ///// --- THIS USES AN EXTERNALLY GENERATED RSA KEY-PiAR
+//    @Bean
+//    public JwtDecoder jwtDecoder() {
+//        return NimbusJwtDecoder.withPublicKey(rsaKeyConfig.publicKey()).build();
+//    }
+//
+//    @Bean
+//    public JwtEncoder jwtEncoder(){
+//        JWK jwk = new RSAKey.Builder(rsaKeyConfig.publicKey()).privateKey(rsaKeyConfig.privateKey()).build();
+//        JWKSource<SecurityContext> jwkSource = new ImmutableJWKSet<>(new JWKSet(jwk));
+//        return new  NimbusJwtEncoder(jwkSource);
+//    }
+
+    @Bean
+    public JWKSource<SecurityContext> jwkSource() {
+        rsaKey = Jwks.generateRsa();
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
     }
 
     @Bean
-    public JwtEncoder jwtEncoder(){
-        JWK jwk = new RSAKey.Builder(rsaKeyConfig.publicKey()).privateKey(rsaKeyConfig.privateKey()).build();
-        JWKSource<SecurityContext> jwkSource = new ImmutableJWKSet<>(new JWKSet(jwk));
-        return new  NimbusJwtEncoder(jwkSource);
+    JwtEncoder jwtEncoder(JWKSource<SecurityContext> jwks) {
+        return new NimbusJwtEncoder(jwks);
     }
 
     @Bean
-    public InMemoryUserDetailsManager user() {
+    JwtDecoder jwtDecoder() throws JOSEException {
+        return NimbusJwtDecoder.withPublicKey(rsaKey.toRSAPublicKey()).build();
+    }
+
+    @Bean
+    public UserDetailsService user() {
         return new InMemoryUserDetailsManager(User.
                 withUsername("richard")
                 .password("{noop}password")
--- src/main/java/com/azubike/ellipsis/springsecuritywithjwt/controller/AuthController.java
+++ src/main/java/com/azubike/ellipsis/springsecuritywithjwt/controller/AuthController.java
@@ -1,10 +1,15 @@
 package com.azubike.ellipsis.springsecuritywithjwt.controller;
 
+import com.azubike.ellipsis.springsecuritywithjwt.model.LoginRequest;
 import com.azubike.ellipsis.springsecuritywithjwt.service.TokenService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,15 +17,30 @@ public class AuthController {
     private final Logger logger = LoggerFactory.getLogger(AuthController.class);
     private final TokenService tokenService;
 
-    public AuthController(final TokenService tokenService) {
+    private final AuthenticationManager authenticationManager;
+
+    public AuthController(final TokenService tokenService, final AuthenticationManager authenticationManager) {
         this.tokenService = tokenService;
+        this.authenticationManager = authenticationManager;
     }
 
+//    @Deprecated
+//    @PostMapping("/token")
+//    public String token(Authentication authentication) {
+//        logger.debug("Requesting authentication for {}", authentication.getName());
+//        String token = tokenService.generateToken(authentication);
+//        logger.debug("Token granted : {}", token);
+//        return token;
+//    }
+
+
     @PostMapping("/token")
-    public String token(Authentication authentication) {
-        logger.debug("Requesting authentication for {}", authentication.getName());
-        String token = tokenService.generateToken(authentication);
-        logger.debug("Token granted : {}", token);
-        return token;
+    public String authenticate(@RequestBody LoginRequest request){
+        System.out.printf("username :  %s password : %s\n" ,request.username() , request.password());
+        final Authentication authenticate = authenticationManager.
+                authenticate(new UsernamePasswordAuthenticationToken(request.username(), request.password()));
+         return tokenService.generateToken(authenticate);
     }
+
+
 }
